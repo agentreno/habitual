2958c6f400a9e3111c68eb3a80b50a7a64942825
Karl <karl.turrell@buto.tv>
2018-02-11 09:48:33 +0000

    Add store creation to index

--- habitual-client/src/index.js
+++ habitual-client/src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { createStore } from 'redux'
+import habitReducer from './reducers'
+
+let store = createStore(habitReducer)
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
