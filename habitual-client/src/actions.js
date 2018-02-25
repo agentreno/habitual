9dc427057d8d2698d34d075dead8112f450d0e1b
Karl <karl.turrell@buto.tv>
2018-02-25 17:43:28 +0000

    Add fetch and receive actions and tests

--- habitual-client/src/actions.js
+++ habitual-client/src/actions.js
@@ -1,7 +1,20 @@
 // Action constants
+export const FETCH_HABITS_REQUEST = 'FETCH_HABITS_REQUEST'
+export const FETCH_HABITS_SUCCESS = 'FETCH_HABITS_SUCCESS'
 export const INCREASE_PROGRESS = 'INCREASE_PROGRESS'
 
 // Action creators
+export function fetchHabits() {
+  return { type: FETCH_HABITS_REQUEST }
+}
+
+export function receiveHabits(data) {
+  return {
+    type: FETCH_HABITS_SUCCESS,
+    habits: data.habits
+  }
+}
+
 export function increaseProgress(id) {
   return { type: INCREASE_PROGRESS, id }
 }
--- habitual-client/src/actions.test.js
+++ habitual-client/src/actions.test.js
@@ -9,4 +9,41 @@ describe('actions', () => {
     }
     expect(actions.increaseProgress(id)).toEqual(expectedAction)
   })
+
+  it('should create an action to fetch habits', () => {
+    const expectedAction = {
+      type: actions.FETCH_HABITS_REQUEST
+    }
+
+    expect(actions.fetchHabits()).toEqual(expectedAction)
+  })
+
+  it('should create an action to receive habits', () => {
+    const data = {
+      habits: [
+        {
+          "id": "1234",
+          "name": "test",
+          "description": "test",
+          "frequency": 1,
+          "progress": 0
+        }
+      ]
+    }
+
+    const expectedAction = {
+      type: actions.FETCH_HABITS_SUCCESS,
+      habits: [
+        {
+          "id": "1234",
+          "name": "test",
+          "description": "test",
+          "frequency": 1,
+          "progress": 0
+        }
+      ]
+    }
+
+    expect(actions.receiveHabits(data)).toEqual(expectedAction)
+  })
 })
