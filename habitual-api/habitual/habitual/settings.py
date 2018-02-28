d35946e7f8437fa0104581399fa398ec3972becf
Karl <karl.turrell@buto.tv>
2018-02-28 08:39:36 +0000

    Add CORS headers and requirements.txt to API

--- habitual-api/habitual/habitual/settings.py
+++ habitual-api/habitual/habitual/settings.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'habitapi'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# CORS
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000'
+)
