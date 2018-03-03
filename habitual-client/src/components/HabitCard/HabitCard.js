84ce0ffde150c53a43d8331a4bda5c12d58cf1dc
Karl <karl.turrell@buto.tv>
2018-03-03 19:32:44 +0000

    Add styled progress bar using library inside HabitCard

--- habitual-client/src/components/HabitCard/HabitCard.js
+++ habitual-client/src/components/HabitCard/HabitCard.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import './Habit.css'
+import React from 'react'
+import { scaleLinear } from 'd3-scale'
+import { Circle } from 'react-progressbar.js'
+import './HabitCard.css'
+
+const colourScale = scaleLinear()
+  .domain([0, 0.33, 0.66, 1])
+  .range(['#fff', '#ff0c00', '#ffbb00', '#61d100'])
+
+const progressBarOptions = {
+  easing: 'easeInOut',
+  strokeWidth: 4,
+  step: (state, circle, attachment) => {
+    circle.path.setAttribute('stroke', colourScale(circle.value()))
+    let value = Math.round(circle.value() * 100)
+    circle.setText(`${value}%`)
+    circle.text.style.color = colourScale(circle.value())
+  }
+}
 
 const Habit = ({ name, description, frequency, progress }) => (
-  <div className="Habit">
-    <h3>{name}</h3>
-    <p>DESCRIPTION: {description}</p>
-    <p>FREQ: {frequency}</p>
-    <p>PROG: {progress}</p>
+  <div className="habitcard">
+    <Circle
+      progress='1'
+      text={'test'}
+      options={progressBarOptions}
+      className={'progressBar'}
+      initialAnimate={true}
+    />
+    <h2>{name}</h2>
+    <p>{description}</p>
   </div>
 )
 
