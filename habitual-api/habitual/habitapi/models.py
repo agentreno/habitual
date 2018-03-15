261b27a17e190315aa1921530abb50a2bba182e1
Karl <karl.turrell@buto.tv>
2018-03-15 21:30:43 +0000

    Add activity entity and Habit computed progress property based on that

--- habitual-api/habitual/habitapi/models.py
+++ habitual-api/habitual/habitapi/models.py
@@ -1,9 +1,28 @@
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
 
 
-# Create your models here.
 class Habit(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     frequency = models.IntegerField(default=1)
-    progress = models.IntegerField(default=0)
+
+    @property
+    def progress(self):
+        # The number of associated activities in the last week
+        return Activity.objects.filter(
+            habit__id=self.id,
+            date__gt=timezone.now() - timedelta(days=7)
+        ).count()
+
+    def __str__(self):
+        return self.name
+
+
+class Activity(models.Model):
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
+    date = models.DateTimeField('date of activity')
+
+    def __str__(self):
+        return f'{self.habit.name} at {self.date}'
