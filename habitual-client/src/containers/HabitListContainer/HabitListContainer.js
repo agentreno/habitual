54b28282273b08b624fd868ffa3c8a5943942634
Karl <karl.hopkinson-turrell@nearform.com>
2018-09-24 23:10:11 +0100

    Replace dispatch function in HabitListContainer

--- habitual-client/src/containers/HabitListContainer/HabitListContainer.js
+++ habitual-client/src/containers/HabitListContainer/HabitListContainer.js
@@ -1,15 +1,20 @@
 import { connect } from 'react-redux'
-import { createActivity } from '../../actions'
+// import { createActivity } from '../../actions'
 import HabitList from '../../components/HabitList/HabitList'
 
 const mapStateToProps = state => ({
   habits: state.habits
 })
 
+/*
 const mapDispatchToProps = dispatch => ({
   onProgress: habit => {
     dispatch(createActivity(habit))
   }
+})
+*/
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  onProgress: ownProps.http('UPDATE_HABIT', 'habits')
 })
 
 const HabitListContainer = connect(
